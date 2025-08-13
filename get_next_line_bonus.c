b5fecce1f52fbbec470dbc84131d92d90ae34d24
Miguel Almeida <micampil@student.42lisboa.com>
2025-08-13 19:47:12 +0100

    Update get_next_line_bonus.c

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,114 +6,57 @@
 /*   By: Micampil <micampil@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 17:00:20 by Micampil          #+#    #+#             */
-/*   Updated: 2025/06/15 18:10:13 by Micampil         ###   ########.fr       */
+/*   Updated: 2025/08/12 20:30:38 by Micampil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 
-char	*ft_join_and_free(char *storage, char *buffer)
-{
-	char	*result;
-
-	result = ft_strjoin(storage, buffer);
-	free(storage);
-	return (result);
-}
-
-char	*ft_extract_line(char *storage)
-{
-	int		i;
-	char	*line;
-
-	i = 0;
-	while (storage[i] && storage[i] != '\n')
-		i++;
-	if (storage[i] == '\n')
-		i++;
-	line = malloc(i + 1);
-	if (!line)
-		return (NULL);
-	line[i] = '\0';
-	while (i--)
-		line[i] = storage[i];
-	return (line);
-}
-
-char	*ft_update_storage(char *storage)
-{
-	int		i;
-	int		j;
-	char	*new_storage;
-
-	i = 0;
-	while (storage[i] && storage[i] != '\n')
-		i++;
-	if (storage[i] == '\n')
-		i++;
-	if (!storage[i])
-	{
-		free(storage);
-		return (NULL);
-	}
-	new_storage = malloc(ft_strlen(storage) - i + 1);
-	if (!new_storage)
-		return (NULL);
-	j = 0;
-	while (storage[i])
-		new_storage[j++] = storage[i++];
-	new_storage[j] = '\0';
-	free(storage);
-	return (new_storage);
-}
-
-char	*read_buffer(int fd, char *storage)
-{
-	char	*buffer;
-	int		bytes_read;
-
-	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
-	if (!buffer)
-		return (NULL);
-	bytes_read = 1;
-	while (!ft_strchr(storage, '\n') && bytes_read > 0)
-	{
-		bytes_read = read(fd, buffer, BUFFER_SIZE);
-		if (bytes_read <= 0)
-			break ;
-		buffer[bytes_read] = '\0';
-		storage = ft_join_and_free(storage, buffer);
-	}
-	free(buffer);
-	return (storage);
-}
-
 char	*get_next_line(int fd)
 {
-	static char *storage[MAX_FD];
-	char *line;
+	static char	buffer[FOPEN_MAX][BUFFER_SIZE + 1];
+	char		*result;
+	int			bytes_read;
 
-	if (fd < 0 || fd >= MAX_FD || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
-	{
-		if (fd >= 0 && fd < MAX_FD)
-		{
-			free(storage[fd]);
-			storage[fd] = NULL;
-		}
+	if (fd < 0 || fd >= FOPEN_MAX || BUFFER_SIZE < 1)
 		return (NULL);
-	}
-	if (!storage[fd])
-		storage[fd] = ft_calloc(1, 1);
-	if (!storage[fd])
-		return (NULL);
-	storage[fd] = read_buffer(fd, storage[fd]);
-	if (!storage[fd] || !*storage[fd])
-	{
-		free(storage[fd]);
-		storage[fd] = NULL;
-		return (NULL);
-	}
-	line = ft_extract_line(storage[fd]);
-	storage[fd] = ft_update_storage(storage[fd]);
-	return (line);
+	result = NULL;
+	bytes_read = 1;
+	if (!buffer[fd][0])
+		bytes_read = -2;
+	return (get_next_line_cont(fd, (char *)buffer, result, bytes_read));
 }
+
+// int	main(int argc, char **argv)
+// {
+// 	int fd;
+// 	int second_fd;
+// 	char *result;
+// 	char *second_result;
+// 	int i;
+
+// 	i = 1;
+// 	if (argc > 1)
+// 	{
+// 		fd = open(argv[1], O_RDONLY);
+// 		second_fd = open(argv[2], O_RDONLY);
+// 		result = get_next_line(fd);
+// 		second_result = get_next_line(second_fd);
+// 		while (result != NULL || second_result != NULL)
+// 		{
+// 			printf("\n\tcall #%d (first fd): %s", i, result);
+// 			printf("\n\tcall #%d (second fd): %s", i, second_result);
+// 			free(result);
+// 			free(second_result);
+// 			result = get_next_line(fd);
+// 			second_result = get_next_line(second_fd);
+// 			i++;
+// 		}
+// 		free(result);
+// 		free(second_result);
+// 		if (fd != -1)
+// 			close(fd);
+// 		if (second_fd != -1)
+// 			close(second_fd);
+// 	}
+// }
