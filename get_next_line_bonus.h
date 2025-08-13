e017ec21abc29a752fc42929f05aef6b899e1d07
Miguel Almeida <micampil@student.42lisboa.com>
2025-08-13 19:46:56 +0100

    Update get_next_line_bonus.h

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,38 +6,28 @@
 /*   By: Micampil <micampil@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 17:00:20 by Micampil          #+#    #+#             */
-/*   Updated: 2025/06/15 18:09:58 by Micampil         ###   ########.fr       */
+/*   Updated: 2025/08/12 22:21:24 by Micampil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 
-# include <stdlib.h>
-# include <unistd.h>
-
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
 # endif
 
-# ifndef MAX_FD
-#  define MAX_FD 1024
-# endif
+# include <fcntl.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-// Main function
 char	*get_next_line(int fd);
-
-// Helper functions
-char	*read_buffer(int fd, char *storage);
-char	*ft_extract_line(char *storage);
-char	*ft_update_storage(char *storage);
-char	*ft_join_and_free(char *storage, char *buffer);
-
-// Utility functions
-size_t	ft_strlen(const char *str);
-char	*ft_strchr(const char *string, int to_find);
-void	ft_bzero(void *s, size_t n);
-void	*ft_calloc(size_t count, size_t size);
-char	*ft_strjoin(char const *s1, char const *s2);
+char	*get_next_line_cont(int fd, char *buffer, char *result, int bytes_read);
+int		find_new_line(char *buff, int fd);
+char	*ft_handle_new_line(char *buff, char *result, int i, int fd);
+char	*ft_process_buffer(char *buff, int i, int fd);
+char	*ft_rezise(char *result, int i, int *r);
 
 #endif
